e3fd1e43a31d16d677985927b4910642b673d8e0
ilyasilkin27 <ilyasilkin27@gmail.com>
2024-10-24 01:00:59 +0300

    remake script, delete 3 files

--- install.sh
+++ install.sh
@@ -7,10 +7,4 @@ then
   cd ..
   rm -r yay
 fi
-if ! ls ~/.local/share  | grep "omf"
-then
-  curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install | fish
-fi
-yay -Syuq --noconfirm --needed stow git neovim github-cli fish zen-browser-bin yazi zoxide p7zip i3-wm autotiling polybar picom-git flameshot ttf-cascadia-code-nerd ttf-nerd-fonts-symbols-mono zscroll-git dunst rofi-power-menu i3lock xss-lock texlive zathura
-cd ~/ScarletFiles
-stow --dotfiles . 
+yay -Syuq --noconfirm --needed neovim zen-browser-bin polybar picom-git flameshot ttf-cascadia-code-nerd ttf-nerd-fonts-symbols-mono dunst rofi-power-menu
